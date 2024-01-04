2671d2ab10b3d5e2c3def726586c512a8d56f41f
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2024-01-04 14:21:48 +0000

    import example

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    n = add(a, b)
-    print(f"{a} + {b} = {n}")
+    total = add(a, b)
+    print("{} + {} = {}".format(a, b, total))
